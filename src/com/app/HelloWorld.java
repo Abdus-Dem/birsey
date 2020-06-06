c25183db385c7dd8f911ca1a71b506b48dd04f22
Abdus-Dem <abdus.demiryurek@gmail.com>
2020-06-06 15:45:33 -0400

    first commit

---
+++ src/com/app/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package com.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
